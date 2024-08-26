5a5ee6bb50579c25d1ab20296a3c0efe2ffd8fb2
DylThePill <118853769+DylThePill@users.noreply.github.com>
2024-08-25 22:07:48 -0400

    Format stuff and update neo

--- src/main/java/io/github/nebulamodding/etultra/EtUltra.java
+++ src/main/java/io/github/nebulamodding/etultra/EtUltra.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package io.github.nebulamodding.etultra;
 
 import org.slf4j.Logger;
 
@@ -35,11 +35,11 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(EtUltra.MODID)
+public class EtUltra
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    public static final String MODID = "et_ultra";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -69,7 +69,7 @@ public class ExampleMod
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public ExampleMod(IEventBus modEventBus, ModContainer modContainer)
+    public EtUltra(IEventBus modEventBus, ModContainer modContainer)
     {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
