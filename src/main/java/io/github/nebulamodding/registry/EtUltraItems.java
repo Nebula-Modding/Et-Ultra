15383e3a10c629ac6f45c26c11269bd46a250505
theognoodle <zclarinet4@gmail.com>
2024-08-28 19:14:41 -0700

    have some more soup

--- src/main/java/io/github/nebulamodding/registry/EtUltraItems.java
+++ src/main/java/io/github/nebulamodding/registry/EtUltraItems.java
@@ -16,18 +16,13 @@ public class EtUltraItems {
     public static final Supplier<BlockItem> COBBLED_MARS_DEEPSLATE = ITEMS.registerSimpleBlockItem(EtUltraBlocks.COBBLED_MARS_DEEPSLATE);
     public static final Supplier<BlockItem> GRAPHITE_BLOCK = ITEMS.registerSimpleBlockItem(EtUltraBlocks.GRAPHITE_BLOCK);
     public static final Supplier<BlockItem> GRAPHITE_DUST = ITEMS.registerSimpleBlockItem(EtUltraBlocks.GRAPHITE_DUST);
+    //
 
 
 
 
 
 
-    public static final Supplier<Item> PENCIL = ITEMS.registerSimpleItem(
-            "pencil",
-            new Item.Properties()
-                    .stacksTo(16)
-                    // The properties to use.
-    );
 
 
 }
