5da2bd07c51152ea27fa0dbddd8ef1d7dcb48c8e
DylThePill <118853769+DylThePill@users.noreply.github.com>
2024-09-13 16:34:45 -0400

    Rewrite EtUltraBlockStateProvider

--- src/main/java/io/github/nebulamodding/datagen/providers/models/EtUltraItemModelProvider.java
+++ src/main/java/io/github/nebulamodding/datagen/providers/models/EtUltraItemModelProvider.java
@@ -1,32 +1,21 @@
 package io.github.nebulamodding.datagen.providers.models;
 
-import io.github.nebulamodding.EtUltra;
 import io.github.nebulamodding.registry.EtUltraItems;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
-import net.minecraft.world.item.Item;
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
-import org.jetbrains.annotations.NotNull;
 
-public class    EtUltraItemModelProvider extends ItemModelProvider {
+public class EtUltraItemModelProvider extends ItemModelProvider {
     public EtUltraItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
-        super(output, EtUltra.MOD_ID, existingFileHelper);
+        super(output, "et_ultra", existingFileHelper);
     }
 
     @Override
     protected void registerModels() {
-        item(EtUltraItems.PENCIL.get());
-    }
+        // Need to set up auto providing
 
-    private void item(Item item) {
-        String name = getItemName(item);
-        getBuilder(name)
-                .parent(getExistingFile(mcLoc("item/generated")))
-                .texture("layer0", "item/" + name);
-    }
+        basicItem(EtUltraItems.PENCIL.get());
 
-    private @NotNull String getItemName(Item item) {
-        return BuiltInRegistries.ITEM.getKey(item).toString().replace(EtUltra.MOD_ID + ":", "");
+
     }
 }
