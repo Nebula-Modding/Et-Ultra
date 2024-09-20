c9dd0f29ee75d9ab8c457287ad443ec67950a55c
DylThePill <118853769+DylThePill@users.noreply.github.com>
2024-09-20 17:55:54 -0400

    Add EtUltraTiers

---
+++ src/main/java/io/github/nebulamodding/registry/EtUltraTiers.java
@@ -0,0 +1,12 @@
+package io.github.nebulamodding.registry;
+
+
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.neoforged.neoforge.common.SimpleTier;
+import net.neoforged.neoforge.common.Tags;
+
+public class EtUltraTiers {
+    public static final Tier DESH = new SimpleTier(Tags.Blocks.NEEDS_WOOD_TOOL, 500, 6, 2, 20, () -> Ingredient.of(Tags.Items.INGOTS_COPPER));
+    public static final Tier OSTRUM = new SimpleTier(Tags.Blocks.NEEDS_WOOD_TOOL, 1000, 7, 3, 20, () -> Ingredient.of(Tags.Items.INGOTS_COPPER));
+}
