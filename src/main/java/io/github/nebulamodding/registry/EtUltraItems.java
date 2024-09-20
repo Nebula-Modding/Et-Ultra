4e672c34b24b653d2907cf609f2a38847c5e4c39
DylThePill <118853769+DylThePill@users.noreply.github.com>
2024-09-20 18:49:42 -0400

    Add ostrum tools

--- src/main/java/io/github/nebulamodding/registry/EtUltraItems.java
+++ src/main/java/io/github/nebulamodding/registry/EtUltraItems.java
@@ -17,23 +17,6 @@ import java.util.function.Supplier;
 public class EtUltraItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(EtUltra.MOD_ID);
 
-    // Tool Tiers
-    public static final Tier DESH_TIER = new SimpleTier(
-            // The tag that determines what blocks this tool cannot break. See below for more information.
-            Tags.Blocks.NEEDS_WOOD_TOOL,
-            // Durability
-            500,
-            // Mining Speed
-            6,
-            // Determines the attack damage bonus. Different tools use this differently. For example, swords do (getAttackDamageBonus() + 4) damage.
-            // Stone uses 1, iron uses 2, corresponding to 5 and 6 attack damage for swords, respectively; our sword does 5.5 damage now.
-            2,
-            // Enchantability
-            20,
-            // Determines the repair ingredient of the tier. Use a supplier for lazy initializing.
-            () -> Ingredient.of(Tags.Items.INGOTS_COPPER)
-    );
-
     public static final DeferredHolders<Item, DeferredItem<? extends Item>> ETRIUM_ITEMS = RegolithItemUtil.registerItems(
             RegolithNeoForge.wrapItems(ITEMS),
             new Item.Properties().rarity(Rarity.RARE),
@@ -63,11 +46,17 @@ public class EtUltraItems {
     public static final DeferredItem<Item> DENSE_PENCIL = ITEMS.registerSimpleItem("dense_pencil", new Item.Properties().stacksTo(16));
 
     // Tools
-    public static final DeferredItem<ShovelItem> DESH_SHOVEL = registerShovelItem("desh_shovel", DESH_TIER, 1, -3);
-    public static final DeferredItem<PickaxeItem> DESH_PICKAXE = registerPickaxeItem("desh_pickaxe", DESH_TIER, 1.5f, -2.8f);
-    public static final DeferredItem<AxeItem> DESH_AXE = registerAxeItem("desh_axe", DESH_TIER, 6, -3.1f);
-    public static final DeferredItem<HoeItem> DESH_HOE = registerHoeItem("desh_hoe", DESH_TIER, -2, -1);
-    public static final DeferredItem<SwordItem> DESH_SWORD = registerSwordItem("desh_sword", DESH_TIER, 3, -2.4f);
+    public static final DeferredItem<ShovelItem> DESH_SHOVEL = registerShovelItem("desh_shovel", EtUltraTiers.DESH, 1, -3);
+    public static final DeferredItem<PickaxeItem> DESH_PICKAXE = registerPickaxeItem("desh_pickaxe", EtUltraTiers.DESH, 1.5f, -2.8f);
+    public static final DeferredItem<AxeItem> DESH_AXE = registerAxeItem("desh_axe", EtUltraTiers.DESH, 6, -3.1f);
+    public static final DeferredItem<HoeItem> DESH_HOE = registerHoeItem("desh_hoe", EtUltraTiers.DESH, -2, -1);
+    public static final DeferredItem<SwordItem> DESH_SWORD = registerSwordItem("desh_sword", EtUltraTiers.DESH, 3, -2.4f);
+
+    public static final DeferredItem<ShovelItem> OSTRUM_SHOVEL = registerShovelItem("ostrum_shovel", EtUltraTiers.OSTRUM, 1, -3);
+    public static final DeferredItem<PickaxeItem> OSTRUM_PICKAXE = registerPickaxeItem("ostrum_pickaxe", EtUltraTiers.OSTRUM, 1.5f, -2.8f);
+    public static final DeferredItem<AxeItem> OSTRUM_AXE = registerAxeItem("ostrum_axe", EtUltraTiers.OSTRUM, 6, -3.1f);
+    public static final DeferredItem<HoeItem> OSTRUM_HOE = registerHoeItem("ostrum_hoe", EtUltraTiers.OSTRUM, -2, -1);
+    public static final DeferredItem<SwordItem> OSTRUM_SWORD = registerSwordItem("ostrum_sword", EtUltraTiers.OSTRUM, 3, -2.4f);
 
     // Helper methods
     private static DeferredItem<ShovelItem> registerShovelItem(String id, Tier tier, float attackDamage, float attackSpeed) {
--- src/main/java/io/github/nebulamodding/registry/EtUltraTiers.java
+++ src/main/java/io/github/nebulamodding/registry/EtUltraTiers.java
@@ -8,5 +8,5 @@ import net.neoforged.neoforge.common.Tags;
 
 public class EtUltraTiers {
     public static final Tier DESH = new SimpleTier(Tags.Blocks.NEEDS_WOOD_TOOL, 500, 6, 2, 20, () -> Ingredient.of(Tags.Items.INGOTS_COPPER));
-    public static final Tier OSTRUM = new SimpleTier(Tags.Blocks.NEEDS_WOOD_TOOL, 1000, 7, 3, 20, () -> Ingredient.of(Tags.Items.INGOTS_COPPER));
+    public static final Tier OSTRUM = new SimpleTier(Tags.Blocks.NEEDS_WOOD_TOOL, 1000, 7, 4, 20, () -> Ingredient.of(Tags.Items.INGOTS_COPPER));
 }
