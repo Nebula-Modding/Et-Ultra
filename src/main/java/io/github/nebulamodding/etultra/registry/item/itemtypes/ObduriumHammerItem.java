36db47cb4d6d5e70b58e5550d4b6c75a23ff3897
DylThePill <118853769+AveryHazel@users.noreply.github.com>
2025-05-12 21:11:09 -0400

    Hammer :D

--- src/main/java/io/github/nebulamodding/etultra/registry/item/itemtypes/ObduriumHammerItem.java
+++ src/main/java/io/github/nebulamodding/etultra/registry/item/itemtypes/ObduriumHammerItem.java
@@ -3,6 +3,8 @@ package io.github.nebulamodding.etultra.registry.item.itemtypes;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.MaceItem;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
@@ -20,29 +22,48 @@ public class ObduriumHammerItem extends MaceItem {
         Block clickedBlock = level.getBlockState(context.getClickedPos()).getBlock();
         assert context.getPlayer() != null;
         if (clickedBlock == Blocks.DAMAGED_ANVIL) {
-            level.setBlock(context.getClickedPos(), Blocks.CHIPPED_ANVIL.defaultBlockState(), 1);
-            level.playSound(context.getPlayer(),
-                    context.getClickedPos(),
-                    SoundEvents.ANVIL_USE,
-                    SoundSource.BLOCKS);
-            context.getPlayer().getOffhandItem().setCount(context.getPlayer().getOffhandItem().getCount()-1);
-            context.getItemInHand().setDamageValue(context.getItemInHand().getDamageValue()+100);
+            if (context.getPlayer().getOffhandItem().getItem() == Items.IRON_INGOT) {
+                level.setBlock(context.getClickedPos(), Blocks.CHIPPED_ANVIL.defaultBlockState(), 1);
+                level.playSound(context.getPlayer(),
+                        context.getClickedPos(),
+                        SoundEvents.ANVIL_USE,
+                        SoundSource.BLOCKS);
+                context.getPlayer().getOffhandItem().setCount(context.getPlayer().getOffhandItem().getCount() - 1);
+                context.getItemInHand().setDamageValue(context.getItemInHand().getDamageValue() + 100);
+            } else {
+                level.setBlock(context.getClickedPos(), Blocks.AIR.defaultBlockState(), 1);
+                level.playSound(context.getPlayer(),
+                        context.getClickedPos(),
+                        SoundEvents.ANVIL_DESTROY,
+                        SoundSource.BLOCKS);
+                context.getItemInHand().setDamageValue(context.getItemInHand().getDamageValue() + 100);
+            }
         } else if (clickedBlock == Blocks.CHIPPED_ANVIL) {
-            level.setBlock(context.getClickedPos(), Blocks.ANVIL.defaultBlockState(), 1);
-            level.playSound(context.getPlayer(),
-                    context.getClickedPos(),
-                    SoundEvents.ANVIL_USE,
-                    SoundSource.BLOCKS);
-            context.getPlayer().getOffhandItem().setCount(context.getPlayer().getOffhandItem().getCount()-1);
-            context.getItemInHand().setDamageValue(context.getItemInHand().getDamageValue()+100);
+            if (context.getPlayer().getOffhandItem().getItem() == Items.IRON_INGOT) {
+                level.setBlock(context.getClickedPos(), Blocks.ANVIL.defaultBlockState(), 1);
+                level.playSound(context.getPlayer(),
+                        context.getClickedPos(),
+                        SoundEvents.ANVIL_USE,
+                        SoundSource.BLOCKS);
+                context.getPlayer().getOffhandItem().setCount(context.getPlayer().getOffhandItem().getCount() - 1);
+                context.getItemInHand().setDamageValue(context.getItemInHand().getDamageValue() + 100);
+            } else {
+                level.setBlock(context.getClickedPos(), Blocks.DAMAGED_ANVIL.defaultBlockState(), 1);
+                level.playSound(context.getPlayer(),
+                        context.getClickedPos(),
+                        SoundEvents.ANVIL_DESTROY,
+                        SoundSource.BLOCKS);
+                context.getItemInHand().setDamageValue(context.getItemInHand().getDamageValue() + 100);
+            }
         } else if (clickedBlock == Blocks.ANVIL) {
-            level.setBlock(context.getClickedPos(), Blocks.IRON_BLOCK.defaultBlockState(), 1);
-            level.playSound(context.getPlayer(),
-                    context.getClickedPos(),
-                    SoundEvents.ANVIL_USE,
-                    SoundSource.BLOCKS);
-            context.getPlayer().getOffhandItem().setCount(context.getPlayer().getOffhandItem().getCount()-1);
-            context.getItemInHand().setDamageValue(context.getItemInHand().getDamageValue()+100);
+            if (context.getPlayer().getOffhandItem().getItem() != Items.IRON_INGOT) {
+                level.setBlock(context.getClickedPos(), Blocks.CHIPPED_ANVIL.defaultBlockState(), 1);
+                level.playSound(context.getPlayer(),
+                        context.getClickedPos(),
+                        SoundEvents.ANVIL_DESTROY,
+                        SoundSource.BLOCKS);
+                context.getItemInHand().setDamageValue(context.getItemInHand().getDamageValue() + 100);
+            }
         }
         return InteractionResult.SUCCESS;
     }
